0bdabc14733992dc38859670fa985e463bd7782d
Konrad Kadzielawa <kkadzi25@gmail.com>
2024-08-20 14:59:10 -0500

    bookstore base template;

---
+++ techbookstore/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for techbookstore project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techbookstore.settings')
+
+application = get_wsgi_application()
